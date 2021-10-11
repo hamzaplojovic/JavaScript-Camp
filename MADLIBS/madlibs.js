fe6432695b49e5a064ca81760a9745a36765de85
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-10-11 19:52:05 +0200

    Deleting MADLIBS game

--- MADLIBS/madlibs.js
+++
@@ -1,6 +0,0 @@
-function wordblanks(firstword, secondword, thirdword) {
-  result = "The " + firstword + " " + secondword + " " + thirdword
-  return result
-
-}
-console.log(wordblanks("big", "dog", "ran"))

3ce6963586da0bad038bfc57981cf46c5fbba9c5
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-10-12 18:12:21 +0200

    For loops trough arrays

--- MAIN/main.js
+++ MAIN/main.js
@@ -145,3 +145,8 @@
     // block of code ();
 //}
 //=============================================================================
+// array = ['item', 'item2', 'item3']
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+    
+// }

71fcab8a3570eb194bd278accd33806e5d31cbc0
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-10-14 20:34:22 +0200

    Adding items in local storage

--- TODO/main.js
+++ TODO/main.js
@@ -3,6 +3,7 @@ toDoContainer = document.getElementById('toDoContainer');
 inputField = document.getElementById('inputField');
 added_button = document.getElementById("added");
 done_button = document.getElementById('done_button')
+clear_button = document.getElementById('clear_button')
 body = document.getElementById("body");
 list_of_paragraphs = []
 done = []
@@ -27,11 +28,13 @@ added_button.addEventListener('click', function(){
     console.clear()
     // console.log("Added: "+list_of_paragraphs)
     var paragraph = document.createElement('p');
-    body.appendChild(paragraph);
-    paragraph.innerText = "Added: "+list_of_paragraphs
+    paragraph.innerText = list_of_paragraphs
+    local_added_paragraph = localStorage.setItem("added_item",paragraph.innerText);
+    console.log("Added in local storage: "+localStorage.getItem("added_item"))
 })
 done_button.addEventListener('click', function(){
-    var done_paragraph2 = document.createElement('p');
-    body.appendChild(done_paragraph2);
-    done_paragraph2.innerText = "Done: "+done
+    localStorage.removeItem("added_item")
+})
+clear_button.addEventListener('click', function(){
+    localStorage.clear()
 })

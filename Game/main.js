c141fd94b56e03d46bc6a8ed5e7741dd6e4bfe15
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-10-16 17:03:47 +0200

    Starting to make a game with js

---
+++ Game/main.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext('2d')
+
+ctx.fillStyle = "green"
+ctx.fillRect(20, 20, 100, 100)
+ctx.fillStyle = "darkolivegreen"
+ctx.fillRect(370, 260, 50, 50)

37fac7a201b5f205c73267545000ad547df6109e
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-10-16 13:56:19 +0200

    I made calculator for terminal only

---
+++ CalculatorTerminal/main.js
@@ -0,0 +1,52 @@
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// console.log("")
+// console.log("1) Addition")
+// console.log("2) Substraction")
+// console.log("3) Multiplication")
+// console.log("4) Dividion")
+// console.log("")
+// rl.question("Enter the option: ", function(option){
+//     if (option == 1){
+//         console.log("")
+//         rl.question("Enter the first number: ", function(num1) {
+//             rl.question("Enter the second number: ", function(num2) {
+//                 console.log(parseInt(num1) + parseInt(num2));
+//                 rl.close();
+//             });
+//         });
+//     }
+//     if (option == 2){
+//         console.log("")
+//         rl.question("Enter the first number: ", function(num1) {
+//             rl.question("Enter the second number: ", function(num2) {
+//                 console.log(parseInt(num1) - parseInt(num2));
+//                 rl.close();
+//             });
+//         });
+//     }
+//     if (option == 3){
+//         console.log("")
+//         rl.question("Enter the first number: ", function(num1) {
+//             rl.question("Enter the second number: ", function(num2) {
+//                 console.log(parseInt(num1) * parseInt(num2));
+//                 rl.close();
+//             });
+//         });
+//     }
+//     if (option == 4){
+//         console.log("")
+//         rl.question("Enter the first number: ", function(num1) {
+//             rl.question("Enter the second number: ", function(num2) {
+//                 console.log(parseInt(num1) / parseInt(num2));
+//                 rl.close();
+//             });
+//         });
+//     }
+// });
+
+

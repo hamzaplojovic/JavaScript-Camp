a6fbb07575b3d9d8fe2dc799d358065a3191b1d3
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-10-19 18:28:50 +0200

    Adding a button for saving palettes

--- COLOR/main.js
+++ COLOR/main.js
@@ -1,5 +1,3 @@
-import * as remote_file from 
-
 localStorage.clear();
 
 container = document.getElementById("container");
@@ -42,4 +40,31 @@ genNew4.addEventListener("click", () => {
 });
 saveButton.addEventListener("click", () => {
   save_palette("color", "color2", "color3", "color4");
+});
+
+var remote_function = palette1.addEventListener("click", () => {
+  document.getElementById("1").style.backgroundColor = localStorage
+    .getItem("colors")
+    .slice(0, 7);
+  document.getElementById("color").innerHTML = localStorage
+    .getItem("colors")
+    .slice(0, 7);
+  document.getElementById("2").style.backgroundColor = localStorage
+    .getItem("colors")
+    .slice(8, 15);
+  document.getElementById("color2").innerHTML = localStorage
+    .getItem("colors")
+    .slice(8, 15);
+  document.getElementById("3").style.backgroundColor = localStorage
+    .getItem("colors")
+    .slice(15, 22);
+  document.getElementById("color3").innerHTML = localStorage
+    .getItem("colors")
+    .slice(15, 22);
+  document.getElementById("4").style.backgroundColor = localStorage
+    .getItem("colors")
+    .slice(22, 29);
+  document.getElementById("color4").innerHTML = localStorage
+    .getItem("colors")
+    .slice(22, 29);
 });

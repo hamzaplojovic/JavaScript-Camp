4acf7c5d93ad2239aee565b44a62e87fb6885869
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-10-30 17:53:27 +0200

    this

--- MAIN/main.js
+++ MAIN/main.js
@@ -428,3 +428,12 @@
 // };
 // var size = Object.size(student);
 // console.log(size);
+
+// const person = {
+//     fname: "Hamza",
+//     lname: "Plojovic",
+//     fullname: function () {
+//         return this.fname + " " + this.lname;
+//     },
+// };
+// console.log(person.fullname);

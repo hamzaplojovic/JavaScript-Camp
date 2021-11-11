cf3d3e3aba74a61f21b4649fb74e82e9cedcc147
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-11-11 11:50:13 +0100

    JavaScript

--- Node/modules.js
+++
@@ -1,8 +0,0 @@
-// const my_module = require("./people");
-// console.log(my_module.people, my_module.ages);
-
-// const { people, ages } = require("./people");
-// console.log(people, ages);
-
-// const os = require("os");
-// console.log(os.platform(), os.homedir());
--- Node/people.js
+++
@@ -1,7 +0,0 @@
-const people = ["john", "mario", "king"];
-const ages = [30, 40, 12];
-
-module.exports = {
-    people,
-    ages,
-};

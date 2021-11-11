53d26bcd69c9ae4fecb1ae011f2bf562defb5a54
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-11-11 11:27:11 +0100

    Server in Node JS

--- Server/main.js
+++ Server/main.js
@@ -4,6 +4,8 @@ var url = require("url");
 http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);
+    var txt = q.season;
+    if (q.season == "winter") {
+        res.write("<h1>" + txt + "<h1>");
+    }
 }).listen(8080);

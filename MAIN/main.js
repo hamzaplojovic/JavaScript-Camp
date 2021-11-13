e4d7a353e8bd7a5dbdfe12ecb498ba426791a99b
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-11-13 13:09:42 +0100

    JS Errors

--- MAIN/main.js
+++ MAIN/main.js
@@ -656,3 +656,48 @@
 // let mycar = new Car("Ford"); // we are running get function
 // mycar.carname = "Volvo"; // we are running set function
 // console.log(mycar.carname);
+
+// ==========================================================================================
+
+// {
+//     const car = "Volvo";
+// }
+// console.log(car);
+
+// ==========================================================================================
+
+// function UserException(message) {
+//     this.message = message;
+//     this.name = "UserException";
+// }
+// function getMonthName(mo) {
+//     mo = mo - 1; // Adjust month number for array index (1 = Jan, 12 = Dec)
+//     var months = [
+//         "Jan",
+//         "Feb",
+//         "Mar",
+//         "Apr",
+//         "May",
+//         "Jun",
+//         "Jul",
+//         "Aug",
+//         "Sep",
+//         "Oct",
+//         "Nov",
+//         "Dec",
+//     ];
+//     if (months[mo] != undefined) {
+//         return months[mo];
+//     } else {
+//         throw new UserException("NoMonthFoundError");
+//     }
+// }
+
+// try {
+//     // statements to try
+//     var myMonth = 15; // 15 is out of bound to raise the exception
+//     var monthName = getMonthName(myMonth);
+// } catch (e) {
+//     monthName = "unknown";
+//     console.error(e.message, e.name); // pass exception object to err handler
+// }

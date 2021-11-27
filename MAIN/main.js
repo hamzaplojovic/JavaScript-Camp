93fbcd245557d4b6e530b0270569a1d02da8dea9
hamzaplojovic <hamzaplojovic4@gmail.com>
2021-11-27 20:04:48 +0100

    Promises

--- MAIN/main.js
+++ MAIN/main.js
@@ -812,3 +812,46 @@
 // }
 
 // processUserInput(greeting);
+
+// JS PROMISES
+// let myPromise = new Promise(function (myResolve, myReject) {
+//     // "Producing Code" (May take some time)
+
+//     myResolve(); // when successful
+//     myReject(); // when error
+// });
+
+// // "Consuming Code" (Must wait for a fulfilled Promise)
+// myPromise.then(
+//     function (value) {
+//         /* code if successful */
+//     },
+//     function (error) {
+//         /* code if some error */
+//     }
+// );
+
+// function myDisplayer(some) {
+//     alert(some);
+// }
+
+// let myPromise = new Promise(function (myResolve, myReject) {
+//     let x = parseInt(prompt("Enter the number"));
+
+//     if (x == 0) {
+//         myResolve("OK");
+//     } else {
+//         myReject("Error");
+//     }
+// });
+
+// myPromise.then(
+//     function (value) {
+//         myDisplayer(value);
+//     },
+//     function (error) {
+//         myDisplayer(error);
+//     }
+// );
+
+// Promise : pending, fullfiled, rejected
